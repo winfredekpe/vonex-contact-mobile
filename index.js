fcee683fc38061d86ea312ac192106ed74e6f17f
winfred <tuekpew@gmail.com>
2020-10-18 16:55:15 +0000

    added animation
    added animation to the under social bar
    with anime js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 add = document.querySelector("#addmessage");
 allmessages = document.querySelector(".chathistory");
 toggler = document.querySelector(".toggler");
-bar = document.querySelector(".bar");
+bar = document.querySelector(".socials");
+istoggled = document.querySelector(".bar");
 
 // adding new message to the messages
 add.addEventListener("keydown", function (event) {
@@ -15,11 +16,30 @@ add.addEventListener("keydown", function (event) {
     allmessages.innerHTML += message;
     this.value = "";
     this.focus();
+    
   }
 });
 
 // removeing the bar for full contacts viewing
-
+shouldtoggle = false
 toggler.addEventListener("click", function () {
-  bar.classList.toggle("badremover");
+if(shouldtoggle === false){
+  // bar.style.display = 'none'
+  anime({
+    targets: bar,
+    scale: 0,
+    // easing: 'easeInOutSine'
+  });
+  
+}else{
+  // bar.style.display = 'flex'
+  anime({
+    targets: bar,
+    scale: 1,
+    // easing: 'easeInOutSine'
+  });
+  
+}
+  shouldtoggle = !shouldtoggle
 });
+
